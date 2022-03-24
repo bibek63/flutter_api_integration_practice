4ea665654739c30f832e78f490e7008c6da85c7f
unknown <36110557+bibek63@users.noreply.github.com>
2022-03-24 08:23:40 +0545

    comment.dart added

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_api_integration/models/comment.dart';
+
 import '../models/post.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +12,20 @@ class RemoteServices {
     if (response.statusCode == 200) {
       var json = response.body;
       return postFromJson(json);
+    }
+    return null;
+  }
+
+  // getting comment function
+  Future<List<Comment>?> getComments() async {
+    var client = http.Client();
+
+    var uri = Uri.parse("https://jsonplaceholder.typicode.com/comments");
+
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return commentFromJson(json);
     }
     return null;
   }
